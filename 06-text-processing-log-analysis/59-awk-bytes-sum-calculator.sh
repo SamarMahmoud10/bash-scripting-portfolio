4d95838775b14fb995d56c4bd80fbc2bdd852743
samar <sm5998144@gmail.com>
2026-08-01 01:04:58 +0300

    modified awk-bytes-sum-calculater add more detailes make it easier to understand

--- 06-text-processing-log-analysis/59-awk-bytes-sum-calculator.sh
+++ 06-text-processing-log-analysis/59-awk-bytes-sum-calculator.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # 1. BEGIN: initialize 'sum' to zero.
+# BEGIN executed one time before processing any input lines.
 # 2. { sum += $5 }: add value in column 5 (bytes) to 'sum' for every line.
 # 3. END: print the final total after processing all lines.
+# END executed one time after processing all input lines.
 awk 'BEGIN { sum = 0 } { sum += $5 } END { print "Total Bytes Transferred:", sum }' access.log
